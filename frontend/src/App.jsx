9c3cd3e383b9c3367150666c47ad10f864209f55
Gangaram1998 <ramggawade1998@gmail.com>
2023-06-16 23:36:59 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { Box, useDisclosure } from '@chakra-ui/react';
 import './App.css';
 import { AllRoutes } from './routes/AllRoutes';
-import Searchpage from './components/Searchpage';
 
 
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <div className="App">
       <Box>
--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import { Register } from '../pages/Register'
